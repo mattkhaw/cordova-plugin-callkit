7e8f6f268cd247ef24eab06a1233bee77e97356e
Danil Chernyshev <Danil.Chernyshev@clarksons.com>
2021-03-11 14:30:52 +0000

    added logs

--- src/ios/CordovaCall.m
+++ src/ios/CordovaCall.m
@@ -401,6 +401,7 @@ NSMutableDictionary* callsMetadata;
         [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler: ^ (BOOL granted)
             {
                 if (granted) {
+                    NSLog(@"mic permission granted");
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Record permission has been granted"];
                 } else {
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Record permission has not been granted"];
@@ -427,6 +428,7 @@ NSMutableDictionary* callsMetadata;
         [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler: ^ (BOOL granted)
             {
                 if (granted) {
+                    NSLog(@"camera permission granted");
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Camera permission has been granted"];
                 } else {
                     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Camera permission has not been granted"];
